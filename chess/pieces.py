80d7cf080d67a786fc769d6b74f7052eecaa42df
JulianBarraganG <xjulianbarragan@gmail.com>
2024-04-27 22:30:23 +0200

    implemented negamax

--- chess/pieces.py
+++ chess/pieces.py
@@ -3,19 +3,11 @@ from .constants import *
 import os
 
 class Piece:
-    def __init__(self, row, col, color : str, name : str, value : float):
-        self.x = row*SQ_SIZE
-        self.y = col*SQ_SIZE
+    def __init__(self, color : str, name : str, value : float):
         self.color = color
         self.name = name
         self.value = value
         self.img = self.get_img()
-        self.pos = self.calc_pos()
-
-    def calc_pos(self):
-        """Returns row and col of piece"""
-        self.pos = (self.x//SQ_SIZE, self.y//SQ_SIZE)
-        return self.pos
     
     def get_img(self):
         img = py.image.load(os.path.join('Assets', f'{self.name}_{self.color}45.png'))
@@ -23,27 +15,27 @@ class Piece:
         return transformed_img
 
 class Pawn(Piece):
-    def __init__(self, row, col, color):
-        super().__init__(row, col, color, 'pawn', 1.)
+    def __init__(self, color):
+        super().__init__(color, 'pawn', 10)
 
 class King(Piece):
-    def __init__(self,  row, col, color):
-        super().__init__( row, col, color, 'king', 10000.)
+    def __init__(self, color):
+        super().__init__(color, 'king', 0)
 
 class Queen(Piece):
-    def __init__(self, row, col, color):
-        super().__init__( row, col, color, 'queen', 9.)
+    def __init__(self,color):
+        super().__init__(color, 'queen', 90)
 
 
 class Rook(Piece):
-    def __init__(self,  row, col, color):
-        super().__init__( row, col, color, 'rook', 5.)
+    def __init__(self, color):
+        super().__init__(color, 'rook', 50)
 
 class Bishop(Piece):
-    def __init__(self,  row, col, color):
-        super().__init__( row, col, color, 'bishop', 3.1)
+    def __init__(self, color):
+        super().__init__(color, 'bishop', 31)
 
 
 class Knight(Piece):
-    def __init__(self,  row, col, color):
-        super().__init__( row, col, color, 'knight', 3.)
+    def __init__(self, color):
+        super().__init__(color, 'knight', 3)
--- engine.py
+++ engine.py
@@ -2,57 +2,71 @@ from chess.game import Game, Move
 from chess.pieces import Queen
 import random
 
-# game = Game()
-# game.board.fen_reader(game, "k7/8/8/8/6p1/5p2/3PP3/K7 b - - 0 1")
-# first_moves = game.get_valid_moves()
+
 CHECKMATE = 10000
 STALEMATE = 0
-DEPTH = 4
+DEPTH = 2
 
 def count_material(game):
+    """
+    Takes the gamestate as input and returns zero-sum game of piece value.
+    Values found in piece.value.
+    """
     count = 0
     board = game.board.array
-    color = 'w' if game.white_to_move else 'b'
-    if game.game_over:
+    if game.game_over: # consider moving this to negamax
         if game.white_to_move:
+            # If white is to move and there is a check mate, white must be checkmated.
             count = (-CHECKMATE) if game.check_mate else 0
         else:
+            # vice versa
             count = CHECKMATE if game.check_mate else 0
     else: 
         for row in board:
             for piece in row:
                 if piece != None:
-                    if piece.color == color:
+                    if piece.color == 'w':
                         count += piece.value
                     else:
                         count -= piece.value
     return count
 
 def eval_position(game):
+    """
+    Evaluates position, currently only based on material.
+    (Ideas for future evaluations: 
+    Positional evaluation as well as threats and protection could add value to pieces.)
+    """
     return count_material(game)
 
-# def negamax(game, moves, turn_multiplier, depth=DEPTH, alpha=-CHECKMATE, beta=CHECKMATE):
-#     global best_move
-#     if depth == 0 or game.game_over:
-#         return turn_multiplier * eval_position(game)
+def negamax(game, moves, turn_multiplier, alpha, beta, depth=DEPTH):
+    """
+    NegaMax is an elaboration of minimax. max(a, b) = -max(-b, -a) is the key idea,
+    making both black and white "maximizing player".
+    Alpha-Beta pruning cuts away unnecessary branches.
+    (To be added: helper function which orders moves by captures, attacks and checks to be considered first.)
+    """
+    global best_move
+
+    if depth == 0 or game.game_over:
+        return turn_multiplier * eval_position(game)
     
-#     max_eval = -CHECKMATE
-#     for move in moves:
-#         game.make_move(move)
-#         subsequent_moves = game.get_valid_moves()
-#         eval = -negamax(game, subsequent_moves, (-turn_multiplier), depth=depth-1, alpha=(-beta), beta=(-alpha))
-#         game.undo_move()
-#         if eval > max_eval:
-#             max_eval = eval
-#             if depth == DEPTH:
-#                 best_move = move
-#         if max_eval > alpha:
-#             alpha = max_eval
-#         if alpha >= beta:
-#             break
-#     return max_eval
+    max_eval = -CHECKMATE
+    for move in moves:
+        game.make_move(move)
+        subsequent_moves = game.get_valid_moves()
+        eval = -negamax(game, subsequent_moves, -turn_multiplier, -beta, -alpha, depth=depth-1)
+        if eval > max_eval:
+            max_eval = eval
+            if depth == DEPTH:
+                best_move = move
+        game.undo_move()
+        alpha = max(alpha, max_eval)
+        if alpha >= beta:
+            break
+    return max_eval
     
-        
+# Predecessor to negamax        
 def minimax(game, moves, maximizing_player, depth=DEPTH, alpha=(-CHECKMATE), beta=CHECKMATE):
     global best_move
     if depth == 0 or game.game_over:
@@ -72,7 +86,7 @@ def minimax(game, moves, maximizing_player, depth=DEPTH, alpha=(-CHECKMATE), bet
                 max_eval = eval
                 if depth == DEPTH:
                     best_move = move
-            alpha = max(alpha, eval)
+            alpha = max(alpha, max_eval)
             if beta <= alpha:
                 break
         return max_eval
@@ -87,20 +101,23 @@ def minimax(game, moves, maximizing_player, depth=DEPTH, alpha=(-CHECKMATE), bet
                 min_eval = eval
                 if depth == DEPTH:
                     best_move = move
-            beta = min(beta, eval)
+            beta = min(beta, min_eval)
             if beta <= alpha:
                 break
         return min_eval
 
 
-
 def random_move(valid_moves):
+    """Returns a random move in moves list"""
     move = random.randint(0, len(valid_moves) - 1)
     return valid_moves[move]
 
 def engine_move(game, moves):
-    #random.shuffle(moves)
-    minimax(game, moves, game.white_to_move)
-    # negamax(game, moves, 1 if game.white_to_move else -1)
+    """
+    Currently based on NegaMax function. 
+    See engine.py
+    """
+    random.shuffle(moves)
+    negamax(game, moves, 1 if game.white_to_move else -1, -CHECKMATE, CHECKMATE)
     move = best_move
     return move
